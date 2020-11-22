4b39f8183f37fd117f46d00bd8d9be2d6a52ac21
William Martín Chávez González <william.chavez@correo.icesi.edu.co>
2020-11-22 11:31:59 -0500

    Node JS Microsoervice example API (#5)
    * NodeJS Microservices API

--- Node1-WebServer-Express/controllers/controller.js
+++ Node1-WebServer-Express/controllers/controller.js
@@ -14,14 +14,14 @@ var operativeSystem = require('os');
 var networkInterfaces = operativeSystem.networkInterfaces();
 var apiServiceLocation = {
     apiServiceName: operativeSystem.hostname(),
-    apiServiceAddress: networkInterfaces[IPv4networkInterface][1].address
+    apiServiceAddress: '192.168 .1 .50'
 };
 
 /**
  * Define the controller functions that will serve as the action listeners to respond to API requests.
  */
 var controller = {
-    microservice: function (req, res) {
+    microservice: function(req, res) {
         var microserviceId = req.params.microserviceId;
         return res.status(200).send({
             "status": "200 OK",
@@ -29,11 +29,11 @@ var controller = {
             "webServerLocation": apiServiceLocation
         });
     },
-    users: function (req, res) {
+    users: function(req, res) {
         console.log('Consultando los usuarios...')
-        // Connect to database:
-        //connection.connect();
-        connection.query('SELECT * FROM `users`', function (error, results, fields) {
+            // Connect to database:
+            //connection.connect();
+        connection.query('SELECT * FROM `users`', function(error, results, fields) {
             if (error) {
                 throw error;
             } else {
@@ -48,14 +48,14 @@ var controller = {
         });
         //connection.end();
     },
-    createUser: function (req, res) {
+    createUser: function(req, res) {
         console.log('Consultando los usuarios...')
-        // Connect to database:
-        //connection.connect();
+            // Connect to database:
+            //connection.connect();
         var userId = parseInt(req.body.id, 10);
         var username = req.body.name;
         var insertSQLStatement = `INSERT INTO users (\` user_id\`, name) VALUES (${userId},'${username}')`;
-        connection.query(insertSQLStatement, function (error, results, fields) {
+        connection.query(insertSQLStatement, function(error, results, fields) {
             if (error) {
                 throw error;
             } else {
@@ -68,7 +68,43 @@ var controller = {
                 });
             }
         });
+    },
+    getCustomerInfo: function(req, res) {
+        console.log(`Consultando la información del cliente: ${req.params.cedula} ...`);
+        return res.status(200).send({
+            "telephonyProvider": {
+                "name": "KONECTA"
+            }
+        });
+    },
+    insertarIlocalizados: function(req, res) {
+        var existeCliente = (req.body.datos[2].propiedad == "id_cliente");
+        var idCliente = req.body.datos[2].valor;
+        console.log(`Reportando la información del contacto ilocalizado con id_cliente: ${idCliente}`);
+        if (existeCliente) {
+            return res.status(200).send({
+                "data": {
+                    "id_respuesta": "11",
+                    "mensaje": `El contacto ilocalizado ${idCliente} se registra sin inconvenientes`,
+                    "codigo": 0,
+                    "estado": "OK"
+                }
+            });
+        } else {
+            return res.status(500).send({
+                "data": {
+                    "id_respuesta": "11",
+                    "mensaje": `El contacto ilocalizado ${idCliente} no se registró`,
+                    "codigo": parseInt(Math.random() * (2 - 0 + 1) + 0),
+                    "estado": "FAILED"
+                }
+            });
+        }
+
     }
+
 };
+
+// Reference https://futurestud.io/tutorials/generate-a-random-number-in-range-with-javascript-node-js
 
 module.exports = controller;
